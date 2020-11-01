9acfd91836276d562e87ea3436632dc33cb0e53d
aduterte <alexduterte@gmail.com>
2020-11-01 16:50:44 -0500

    questions done

--- db/seeds.rb
+++ db/seeds.rb
@@ -214,7 +214,204 @@ Question.create([
     {
         question: "What are two rights of everyone living in the United States?",
         answers: ["freedom of expression","freedom of speech","freedom of assembly","freedom to petition the government","freedom of religion","the right to bear arms"]
+    },
+    {
+        question: "What do we show loyaltyto when we say the Pledge of Allegiance?",
+        answers: ["the United States", "the flag"]
+    },
+    {
+        question: "What is one promise you make when you become a United States citizen?",
+        answers: ["give up loyalty to other countries","defend the Constitution and laws of the United States","obey the laws of the United States","serve in the U.S. military (if needed)","be loyal to the United States"]
+    },
+    {
+        question: "How old do citizens have to be to vote for President?",
+        answers: ["eighteen ( 18 ) and older"]
+    },
+    {
+        question: "What are two ways that Americans can participate in their democracy?",
+        answers: ["vote","join a political party","help with a campaign","join a civic group","give an elected official your opinion on an issue", "call Senators and Representatives","publicly support or oppose an issue or policy","run for office","write to a newspaper"]
+    },
+    {
+        question: "When is the last day you can send in federal income tax forms?",
+        answers: ["April 15"]
+    },
+    {
+        question: "When must all men register for the Selective Service",
+        answers: ["at age eighteen ( 18) ","between eighteen (18) and twenty-six(26)"]
+    },
+    {
+        question: "What is on reason colonists came to America?",
+        answers: ["freedom","political liberty","religous freedom","ecomnomic oppurtinity","practice their religion","escape persecution"]
+    },
+    {
+        question: "Who lived in America before Europeans arrived?",
+        answers: ["American Indians","Native Americans"]
+    },
+    {
+        question: "What group of people was taken to America and sold as slaves?",
+        answers: ["Africans","people from Africa"]
+    },
+    {
+        question: "Why did colonists fight the British?",
+        answers: ["because of high taxes (taxation without representation","because the British army stayed in their houses (boarding, quartering)","because they didn't have self-government"]
+    },
+    {
+        question: "Who wrote the Declaration of Independence?",
+        answers: ["(Thomas) Jefferson"]
+    },
+    {
+        question: "When was the Declaration of Independence adopted?",
+        answers: ["July 4, 1776"]
+    },
+    {
+        question: "There were 13 original states. Name three.",
+        answers: ["New Hampshire","Massachusetts","Rhode Island","Connecticut","New York","New Jersey","Pennsylvania","Delaware","Maryland","Virginia","North Carolina","South Carolina","Georgia"]
+    },
+    {
+        question: "What happend at the Constitutional Convention?",
+        answers: ["The Constitution was written.","The Founding Fathers wrote the Constitution."]
+    },
+    {
+        question: "When was the Constitution written?",
+        answers: ["1787"]
+    },
+    {
+        question: "The Federalist Papers supported the passage of the U.S. Constitution. Name one of the writers.",
+        answers: ["(James) Madison","(Alexander) Hamilton","(John) Jay","Publius"]
+    },
+    {
+        question: "What is one thing Benjamin Franklin is famous for?",
+        answers: ["U.S. Diplomat","oldest member of the Constitutional Convention","first Postmaster General of the United States","writer of 'Poor Richard's Almanac'","started the first free libraries"]
+    },
+    {
+        question: "Who is the 'Father of Our Country'?",
+        answers: ["(George) Washington"]
+    },
+    {
+        question: "Who was the first President?",
+        answers: ["(George) Washington"]
+    },
+    {
+        question: "What territory did the United States buy from France in 1803?",
+        answers: ["the Louisiana Territory", "Louisiana"]
+    },
+    {
+        question: "Name one war fought by the United States in 1800s",
+        answers: ["War of 1812","Mexican-American War","Civil War","Spanish-American War"]
+    },
+    {
+        question: "Name of the U.S. war between the North and South.",
+        answers: ["the Civl War","the War between the states"]
+    },
+    {
+        question: "Name one problem that led to the Civil War.",
+        answers: ["slavery","economic","state's rights"]
+    },
+    {
+        question: "What was one important thing that Abraham Lincoln did?",
+        answers: ["freed the slaves (Emancipation Proclamation)","saved (or preserved) the Union","led the United States during the Civil War"]
+    },
+    {
+        question: "What did the Emancipation Proclamation do?",
+        answers: ["freed the slaves","freed slaves in the Confederacy","freed slaves in the Confederate states","freed slaves in most Southern states"]
+    },
+    {
+        question: "What did Susan B. Anthony do?",
+        answers: ["fought for women's rights","fought for civil rights"]
+    },
+    {
+        question: "Name one war fought by the United States in the 1900s.",
+        answers: ["World War I","World War II","Korean","Vietnam War", "(Persian) Gulf War"]
+    },
+    {
+        question: "Who was President during World War I?",
+        answers: ["(Woodrow) Wilson"]
+    },
+    {
+        question: "Who was President during the Great Depression and World War II?",
+        answers: ["(Franklin) Roosevelt"]
+    },
+    {
+        question: "Who did the United States fight in World War II",
+        answers: ["Japan, Germany, and Italy"]
+    },
+    {
+        question: "Before he was President, Eisenhower was a general. What war was he in?",
+        answers: ["World War II"]
+    },
+    {
+        question: "During the Cold War, what was the main concern of the United States?",
+        answers: ["Communism"]
+    },
+    {
+        question: "What movement tried to end racial dsicrimination",
+        answers: ["civil rights (movement)"]
+    },
+    {
+        question: "What did Martin Luther King, Jr. do?",
+        answers: ["fought for civil rights","worked for equality for all Americans"]
+    },
+    {
+        question: "What major event happend on September 11, 2001, in the United States?",
+        answers: ["Terrorists attacked the United States."]
+    },
+    {
+        question: "Name one American tribe in the United States.",
+        answers: ["Cherokee,","Navajo","Sioux","Chippewa","Choctaw","Pueblo","Apache","Iroquois","Creek","Blackfeet","Seminole","Cheyenne","Arawak","Shawnee", "Mohegan","Huron","Oneida","Lakota","Crow","Teton","Hopi","Inuit"]
+    },
+    {
+        question: "Name one of the two longest rivers in the United States.",
+        answers: ["Missouri (River)","Mississippi (River)"]
+    },
+    {
+        question: "What ocean is on the West Coast of the United States?",
+        answers: ["Pacific (Ocean)"]
+    },
+    {
+        question: "What ocean is on the East Coast of the United States?",
+        answers: ["Atlantic (Ocean)"]
+    },
+    {
+        question: "Name one U.S. territory.",
+        answers: ["Puerto Rico","U.S. Virgin Islands","American Samoa","Northern Mariana Islands","Guam"]
+    },
+    {
+        question: "Name one state that borders Canada.",
+        answers: ["Maine","New Hampshire","Vermont","New York","Pennsylvania","Ohio","Michigan","Minnesota","North Dakota","Montana","Idaho","Washington","Alaska"]
+    },
+    {
+        question: "Name one state that borders Mexico.",
+        answers: ["California","Arizona","New Mexico","Texas"]
+    },
+    {
+        question: "What is the capital of the United States?",
+        answers: ["Washington, D.C."]
+    },
+    {
+        question: "Where is the Statue of Liberty?",
+        answers: ["New York ( Harbor )","Liberty Island","[Also acceptable are New Jersey, near New York City, and on the Hudson (River).]"]
+    },
+    {
+        question: "Why does the flag have 13 stripes?",
+        answers: ["because there were 13 original colonies","because the stripes represent the original colonies"]
+    },
+    {
+        question: "Why does the flag have 50 stars?",
+        answers: ["because there is one star for each state","because each star represents a state","because there are 50 states"]
+    },
+    {
+        question: "What is the name of the national anthem?",
+        answers: ["The Star-Spangled Banner"]
+    },
+    {
+        question: "When do we celebrate Independence Day?",
+        answers: ["July 4"]
+    },
+    {
+        question: "Name two national U.S. holidays",
+        answers: ["New Year's Day","Martin Luther King, Jr. Day","President's Day","Memorial Day","Independence Day","Labor Day","Columbus Day","Veterans Day","Thanksgiving","Christmas"]
     }
+    
 ])
 
 # Question.create(
