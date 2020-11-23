429cbf054d4323115240e602654d74762bab26a7
aduterte <alexduterte@gmail.com>
2020-11-22 20:20:45 -0500

    added user class

---
+++ db/migrate/20201123012029_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password_digest
+      t.string :city
+      t.string :address
+      t.string :zipcode
+
+      t.timestamps
+    end
+  end
+end
